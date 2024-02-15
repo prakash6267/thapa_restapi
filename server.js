fe0bc4ba3596ded24704c94b351e5709c63b4ac7
Prakash Singh <psingh41824@gmail.com>
2024-02-15 22:33:17 +0530

    Your descriptive commit message here

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const app = require('./app')
-const port = process.env.PORT || 4000
+const port =  3000
 const server = http.createServer(app);
-
 
 server.listen(port,console.log("server is running"));
